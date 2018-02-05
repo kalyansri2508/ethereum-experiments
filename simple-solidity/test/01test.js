4cba3dcedd32b368105e327847aba4835499e0d4
kalyansri2508 <kalyansri2508@gmail.com>
2018-02-05 21:50:06 +0800

    added truffle tests for SimpleContract

---
+++ simple-solidity/test/01test.js
@@ -0,0 +1,44 @@
+var SimpleContract = artifacts.require('SimpleContract');
+
+var expect = require('chai').expect;
+
+contract("Test the SimpleContract",(accounts)=>{
+    describe("Deploy the SimpleContract",()=>{
+      it("Catch the instance of SimpleContract",()=>{
+          return SimpleContract.new().then((instance)=>{
+              simpleContract = instance;
+          });
+      });
+    });
+
+    describe("Check the variables",()=>{
+      it("Call the setName Function - Lattala",()=>{
+        return simpleContract.setName("Lattala").then((result)=>{
+          console.log(result);
+          expect(result).to.not.be.equal("error");
+        });
+      });
+
+      it("Call the getName Function - Lattala",()=>{
+        return simpleContract.getName().then((result)=>{
+          console.log(result);
+          expect(result).to.be.equal("Lattala");
+        });
+      });
+
+      it("Call the setName Function - Surya",()=>{
+        return simpleContract.setName("Surya").then((result)=>{
+          console.log(result);
+          expect(result).to.not.be.equal("error");
+        });
+      });
+
+      it("Call the getName Function - Surya",()=>{
+        return simpleContract.getName().then((result)=>{
+          console.log(result);
+          expect(result).to.be.equal("Surya");
+        });
+      });
+
+    });
+});
